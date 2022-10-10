d6c72924052c754cf785581da016b4f7d7dfbd96
jojimanos <manosgeorge7@gmail.com>
2022-10-10 23:55:58 +0300

    stem_mod modification

--- pages/components/stem_mod.js
+++ pages/components/stem_mod.js
@@ -4,11 +4,16 @@ function Stem_mod(verb) {
     verb.match(/.ω$/) ? declension = "thematic" : declension;
     verb.match(/.μι$/) ? declension = "athematic": declension;
     let stem = null 
-    verb.match(/^ά/|/.ά./) ? stem = verb.replace("ά", "α") : stem = verb
-    verb.match(/^έ/|/.έ./) ? stem = verb.replace("έ", "ε") : stem
-    verb.match(/^ί/|/.ί./) ? stem = verb.replace("ί", "ι") : stem 
-    verb.match(/^ό/|/.ό./) ? stem = verb.replace("ό", "ο") : stem
-    verb.match(/^ύ/|/.ύ./) ? stem = verb.replace("ύ", "υ") : stem
+    verb.match(/^ά/) ? stem = verb.replace("ά", "α") : stem = verb
+    verb.match(/.ά./) ? stem = verb.replace("ά", "α") : stem
+    verb.match(/^έ/) ? stem = verb.replace("έ", "ε") : stem
+    verb.match(/.έ./) ? stem = verb.replace("έ", "ε") : stem
+    verb.match(/^ί/) ? stem = verb.replace("ί", "ι") : stem
+    verb.match(/.ί./) ? stem = verb.replace("ί", "ι") : stem 
+    verb.match(/^ό/) ? stem = verb.replace("ό", "ο") : stem
+    verb.match(/.ό./) ? stem = verb.replace("ό", "ο") : stem
+    verb.match(/^ύ/) ? stem = verb.replace("ύ", "υ") : stem
+    verb.match(/.ύ./) ? stem = verb.replace("ύ", "υ") : stem
 
     if (declension = "thematic") {
         stem = stem.slice(0, -1)
